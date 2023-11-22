20419317c472cb4c01852f0b1b1a06aed59d8924
jcbhmr <jcbhmr@outlook.com>
2023-11-22 13:15:49 -0600

    try this

--- main.sh
+++ main.sh
@@ -5,9 +5,11 @@ set -e
 protocol=$(echo "$GITHUB_SERVER_URL" | cut -d/ -f1)
 host=$(echo "$GITHUB_SERVER_URL" | cut -d/ -f3)
 git clone "$protocol//x:$INPUT_TOKEN@$host/$GITHUB_REPOSITORY.git" \
-  "$RUNNER_TEMP/gh-pages" --depth 1 --branch gh-pages
+  "$RUNNER_TEMP/gh-pages" --depth 1
 
 pushd "$RUNNER_TEMP/gh-pages"
+
+git switch gh-pages 2>/dev/null || git switch -c gh-pages
 
 if [[ $INPUT_PREVIEW == true ]]; then
   rsync -av --delete --exclude=.git --exclude=.github "$PAGESDATA" "./preview-$GITHUB_RUN_ID/"
@@ -18,16 +20,18 @@ rm -rf "$PAGESDATA"
 
 touch .nojekyll
 
-git add --all --force
 git config --local author.name "github-actions[bot]"
 git config --local author.email "github-actions[bot]@github.com"
+
+git add --all --force
 git commit --allow-empty --message "Deploy pages"
-git push origin gh-pages
 
 find . -name 'preview-*' -ctime +90 -exec rm -rf {} \;
+
 git add --all --force
 git commit --allow-empty --message "Remove old previews"
-git push origin gh-pages
+
+git push origin HEAD:gh-pages
 
 popd
 rm -rf "$RUNNER_TEMP/gh-pages"
